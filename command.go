c555607a5d0c32b1c5e81052ca9041634ae0d629
Stefano Da Ros <sdaros@cip.li>
2015-09-15 00:12:43 +0200

    testing basic stamp functionality in main

---
+++ command.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"encoding/hex"
+)
+
+func main() {
+	secret := &secret{"https://facebook.com", "p@ssw0rd", "username", "note"}
+	tag := tag("facebook")
+	stamp := &stamp{stamper.lick(tag)}
+	fmt.Printf("Secret: %v,\nSealed Secret (Stamp): %v,\n",
+	secret, hex.EncodeToString(stamp.content))
+	seal := sealer.seal(secret)
+	fmt.Printf("Seal: %v", seal)
+}
---
+++ courier.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+)
+
+func getPassword() []byte {
+	fd := int(os.Stdin.Fd())
+	pass, err := terminal.ReadPassword(fd)
+	if err != nil {
+		panic(err)
+	}
+	return pass
+}
