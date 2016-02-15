9e15ebbb7af1262088b2f3cdb8c14c37be559325
Stefano Da Ros <sdaros@cip.li>
2016-02-15 08:32:13 +0100

    added test cases using goConvey for PasswordCommand

--- cmd/generateCommand_test.go
+++ cmd/generateCommand_test.go
@@ -1,3 +1,4 @@
+// NEXT: implement tests
 package cmd
 
 import (
--- cmd/passwordCommand_test.go
+++ cmd/passwordCommand_test.go
@@ -2,12 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 	"unicode"
 )
 
-// NEXT: refactor test to use table
-// see github.com/golang/go/wiki/TableDrivenTests
+// NEXT: finish test suite with goconvey
 type testVector string
 
 // testVector must implement fmt.Stringer
@@ -15,78 +15,75 @@ func (tv testVector) String() string {
 	return fmt.Sprintf("%v", string(tv))
 }
 
-func Test_password_against_invalid_password_length(t *testing.T) {
-	command := NewPassword()
-	exec := command.execute
+func TestPasswordAppliesCommandFlagsProperly(t *testing.T) {
+	Convey("Given a Password Command to execute", t, func() {
+		command := NewPassword()
+		commandExecuteFunc := command.execute
 
-	// passwordLengthFlag too short
-	command.passwordLength.value = 0
-	_, err := exec()
-	if err == nil {
-		t.Error("Should have received an error")
-	}
-	// passwordLengthFlag too long
-	command.passwordLength.value = 257
-	_, err = exec()
-	if err == nil {
-		t.Error("Should have received an error")
-	}
+		Convey("When no flags to the command are passed", func() {
+			cmdResult, err := commandExecuteFunc()
+
+			Convey("It should apply the default values of the flags", func() {
+
+				So(err, ShouldBeNil)
+				So(len(cmdResult.String()), ShouldEqual, 15)
+				So(passwordContainsSymbols(cmdResult), ShouldEqual, true)
+
+			})
+
+		})
+		Convey("When the value of password-length flag is 256", func() {
+			command.passwordLength.value = 256
+
+			Convey("The result of the command should be a password "+
+				"with length of 256 characters", func() {
+				cmdResult, err := commandExecuteFunc()
+
+				So(err, ShouldBeNil)
+				So(len(cmdResult.String()), ShouldEqual, 256)
+
+			})
+
+		})
+
+		Convey("When the value of no-symbols flag is true", func() {
+			command.noSymbols.value = true
+
+			Convey("The result of the command should be a password "+
+				"that does not contain any symbols (is "+
+				"only alphanumeric [A-Za-z])", func() {
+				cmdResult, err := commandExecuteFunc()
+
+				So(err, ShouldBeNil)
+				So(passwordContainsSymbols(cmdResult), ShouldEqual, false)
+			})
+		})
+
+		Convey("When the value of no-symbols flag is false", func() {
+			command.noSymbols.value = false
+
+			Convey("The result of the command should be a password "+
+				"that *contains* symbols (see cmd.PasswordCommand)", func() {
+				cmdResult, err := commandExecuteFunc()
+
+				So(err, ShouldBeNil)
+				So(passwordContainsSymbols(cmdResult), ShouldEqual, true)
+			})
+		})
+
+	})
+
 }
 
-func Test_password_matches_a_provided_length(t *testing.T) {
-	command := NewPassword()
-	exec := command.execute
-
-	command.passwordLength.value = 256
-	command.noSymbols.value = false
-	result, err := exec()
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-	if result.String() == "" || len(result.String()) != 256 {
-		t.Errorf("Expected positive non nil length of password")
-	}
-}
-
-func Test_password_with_and_without_symbols(t *testing.T) {
-	command := NewPassword()
-	exec := command.execute
-
-	// password should be generated with symbols
-	command.passwordLength.value = 256
-	command.noSymbols.value = false
-	result, err := exec()
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
+func passwordContainsSymbols(c *CmdResult) bool {
 	isSymbolFound := false
-	for _, r := range result.String() {
+	for _, r := range c.String() {
 		if unicode.IsSymbol(r) {
 			isSymbolFound = true
 			break
 		}
 	}
-	if !isSymbolFound {
-		t.Error("Expected to find a symbol in password")
-	}
-	// password should be generated without symbols
-	command.passwordLength.value = 256
-	command.noSymbols.value = true
-	result, err = exec()
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-	isSymbolFound = false
-	for _, r := range result.String() {
-		if unicode.IsSymbol(r) {
-			isSymbolFound = true
-			break
-		}
-	}
-	if isSymbolFound {
-		t.Error("Expected not to find a symbol in password")
-	}
-
+	return isSymbolFound
 }
 
 func Test_password_with_default_flags(t *testing.T) {
